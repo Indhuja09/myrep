c09414e50f0149b600d762d87f9c3301e0c268b6
Indhuja09 <32064526+Indhuja09@users.noreply.github.com>
2018-01-18 21:43:35 +0000

    Modulus/Dividing by 7
    Write a program that asks the user for an integer number and then prints out the remainder after the number is divided by 7.

---
+++ modulo.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter an integer number:"))
+number2 = 7
+
+division = number1/number2
+remainder = number1%number2
+
+print("The remainder is", remainder)
+print("The number 7 fits into", number1, division, "times")
