fb1ec5eeb9c739d4bac082dbcbd81b40ccb65d6e
Indhuja09 <32064526+Indhuja09@users.noreply.github.com>
2018-01-18 21:40:38 +0000

    Splitting a string
    Split the sentence into a list of words

---
+++ splitting strings.py
@@ -0,0 +1,8 @@
+print("Enter 2 short sentences")
+sentence1 = input("Enter your first sentence")
+sentence2 = input("Enter your second sentence")
+
+words1 = sentence1.split()
+words2 = sentence2.split()
+
+print(words1+words2)
