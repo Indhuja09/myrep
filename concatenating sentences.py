a23d857b3d5c9e9a1cb793e3a55458473625116f
Indhuja09 <32064526+Indhuja09@users.noreply.github.com>
2018-01-19 10:57:11 +0000

    Update concatenating sentences.py

--- concatenating sentences.py
+++ concatenating sentences.py
@@ -2,12 +2,4 @@ print("Enter 2 short sentences")
 sentence1 = input("Enter your first sentence")
 sentence2 = input("Enter your second sentence")
 
-words1 = sentence1.split()
-words2 = sentence2.split()
-
-print(words1+words2)
-
-sortlistofword=sorted(words1+words2) 
-print(sortlistofword) 
-
-print(len(words1+words2))
+print(sentence1+sentence2)
